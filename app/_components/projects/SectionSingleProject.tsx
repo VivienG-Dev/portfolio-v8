3c5cf0f0c5cb3bb162d6448f2cd6ef464bd2616c
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-07-17 13:49:31 +0200

    refactor: improved the data fetching

--- app/_components/projects/SectionSingleProject.tsx
+++ app/_components/projects/SectionSingleProject.tsx
@@ -2,32 +2,28 @@ import { Section } from "../Section";
 import { Title } from "../Title";
 import { Card } from "@/components/ui/card";
 import { SingleProject } from "./SingleProject";
-import { getAllProjects, getProjectById, Project } from "@/lib/projects";
+import { getProjectById } from "@/lib/projects";
 import { BreadcrumbComponent } from "./Breadcrumb";
 
 interface SectionSingleProjectProps {
-  projectId?: string;
+  projectId: string;
 }
 
 export const SectionSingleProject = ({ projectId }: SectionSingleProjectProps) => {
-  let projects: Project[];
+  const project = getProjectById(projectId);
 
-  if (projectId) {
-    const project = getProjectById(projectId);
-    projects = project ? [project] : [];
-  } else {
-    projects = getAllProjects();
-  }
+  const renderProjectContent = () => {
+    if (!project) {
+      return <div>Project not found</div>;
+    }
+    return <SingleProject project={project} />;
+  };
 
   return (
     <Section className="flex flex-col gap-4" title="Side Projects" rotate="-rotate-90" position="-right-20">
       <BreadcrumbComponent projectId={projectId} />
       <Title title="Project Details" />
-      <Card className="flex flex-col flex-wrap gap-4 p-4">
-        {projects.map((project) => (
-          <SingleProject key={project.id} project={project} />
-        ))}
-      </Card>
+      <Card className="flex flex-col flex-wrap gap-4 p-4">{renderProjectContent()}</Card>
     </Section>
   );
 };
