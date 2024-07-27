f800925751e231fdb41a06c0548002af3d4b2d44
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-07-27 18:32:38 +0200

    style: fixed the height of the skills card to be at the same height

--- app/_components/skills/SectionSkills.tsx
+++ app/_components/skills/SectionSkills.tsx
@@ -25,22 +25,37 @@ export const SectionSkills = () => {
   return (
     <Section className="flex flex-col items-start gap-4" title="Compétences" rotate="rotate-90" position="-left-20">
       <Title title="Compétences" />
-      <div ref={containerRef} className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <motion.div variants={createVariants(0)} initial="hidden" animate={isInView ? "visible" : "hidden"}>
+      <div ref={containerRef} className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full">
+        <motion.div
+          className="h-full flex"
+          variants={createVariants(0)}
+          initial="hidden"
+          animate={isInView ? "visible" : "hidden"}
+        >
           <Skills
             title="Front-end"
             description="Pour le front-end, mon framework principale reste Vue.js en utilisant la Composition API. Je travaille également avec Nuxt.js mais il m'arrive de travailler avec Next.js !"
             technologies={["Vue.js", "Nuxt.js", "Next.js", "Composition API"]}
           />
         </motion.div>
-        <motion.div variants={createVariants(0.2)} initial="hidden" animate={isInView ? "visible" : "hidden"}>
+        <motion.div
+          className="h-full flex"
+          variants={createVariants(0.2)}
+          initial="hidden"
+          animate={isInView ? "visible" : "hidden"}
+        >
           <Skills
             title="Back-end"
             description="Pour le back-end, j'utilise des technologies comme Node.js associée à Express. J'utilise MySQL ou PostgreSQL pour la base de données."
             technologies={["Node.js", "Express", "MySQL", "PostgreSQL"]}
           />
         </motion.div>
-        <motion.div variants={createVariants(0.4)} initial="hidden" animate={isInView ? "visible" : "hidden"}>
+        <motion.div
+          className="h-full flex"
+          variants={createVariants(0.4)}
+          initial="hidden"
+          animate={isInView ? "visible" : "hidden"}
+        >
           <Skills
             title="Design"
             description="Pour le design, j'adore utiliser TailwindCSS. Je peux évidemment aussi utiliser SASS au besoin. J'utilise aussi Figma pour les maquettes."
