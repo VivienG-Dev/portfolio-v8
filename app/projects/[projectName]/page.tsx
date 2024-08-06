3a95b49c1a2695c87762175600da1c06a02daf05
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-08-07 00:18:40 +0200

    chore: adding dynamic metatags to the single projects

--- app/projects/[projectName]/page.tsx
+++ app/projects/[projectName]/page.tsx
@@ -6,17 +6,59 @@ import { SectionSingleProject } from "@/app/_components/projects/SectionSinglePr
 import { Footer } from "@/app/_components/Footer";
 
 // Use getProjectById to get the data of the project to add in the metadata
-export async function generateMetadata({ params }: { params: { projectName: string } }) {
+
+export async function generateMetadata({ params }: { params: { projectName: string } }): Promise<Metadata> {
   const project = getProjectById(params.projectName);
+
   if (!project) {
     return {
       title: "Side project not found",
       description: "Side project not found",
     };
   }
+
+  const title = `${project.title} | VivienG`;
+  const description = project.shortDescription;
+  const url = `https://www.vivieng.com/projects/${params.projectName}`;
+  const keywords = project.technologies;
+
   return {
-    title: project.title,
-    description: project.shortDescription,
+    title,
+    description,
+    keywords: keywords,
+    openGraph: {
+      title,
+      description,
+      url,
+      siteName: "Développeur web | VivienG",
+      images: [
+        {
+          url: `https://www.vivieng.com/images/${project.imageUrl}`,
+          width: 1200,
+          height: 630,
+          alt: `${project.title} Preview`,
+        },
+      ],
+      locale: "fr_FR",
+      type: "article",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [`https://www.vivieng.com/images/${project.imageUrl}`], // Again, consider using a project-specific image
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
   };
 }
 
