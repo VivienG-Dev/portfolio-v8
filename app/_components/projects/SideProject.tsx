4835acdf3ac1277e35762662a0b02cffd6071a38
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-08-08 02:15:16 +0200

    chore: changed links to the single projects page

--- app/_components/projects/SideProject.tsx
+++ app/_components/projects/SideProject.tsx
@@ -14,7 +14,7 @@ export const SideProject = ({ title, shortDescription, id, imageUrl }: SideProje
   return (
     <div className="flex md:flex-col items-center gap-2 flex-1 shadow md:shadow-none bg-card border border-1 md:border-none md:bg-transparent rounded-lg p-4 xl:p-0 hover:shadow-lg md:hover:shadow-none transition-shadow duration-300 ">
       <div className="flex-shrink-0 bg-transparent md:bg-card rounded-lg flex items-center justify-center overflow-hidden p-0 md:p-4 shadow hover:shadow-lg transition-shadow duration-300">
-        <Link href={`/projects/${id}`}>
+        <Link href={`/project/${id}`}>
           <Image
             src={imageUrl}
             alt={title}
@@ -27,7 +27,7 @@ export const SideProject = ({ title, shortDescription, id, imageUrl }: SideProje
         </Link>
       </div>
       <div className="text-center w-[70%]">
-        <Link href={`/projects/${id}`}>
+        <Link href={`/project/${id}`}>
           <p className="text-lg font-semibold">{title}</p>
         </Link>
         <p className="text-sm text-muted-foreground">{shortDescription}</p>
