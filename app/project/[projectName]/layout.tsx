5b92538c8e7839923413013998fd900de4d18e01
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-08-09 14:22:46 +0200

    chore: adding layout for the single projects page

---
+++ app/project/[projectName]/layout.tsx
@@ -0,0 +1,61 @@
+import { Metadata } from "next";
+import { getProjectById } from "@/lib/projects";
+
+export async function generateMetadata({ params }: { params: { projectName: string } }): Promise<Metadata> {
+  const project = getProjectById(params.projectName);
+
+  if (!project) {
+    return {
+      title: "Side project not found",
+      description: "Side project not found",
+    };
+  }
+
+  const title = `${project.title} | VivienG`;
+  const description = project.shortDescription;
+  const url = `https://www.vivieng.com/project/${params.projectName}`;
+  const keywords = project.technologies;
+
+  return {
+    title,
+    description,
+    keywords: keywords,
+    openGraph: {
+      title,
+      description,
+      url,
+      siteName: "Développeur web | VivienG",
+      images: [
+        {
+          url: `https://www.vivieng.com${project.imageUrl}`,
+          width: 1200,
+          height: 630,
+          alt: `${project.title} Preview`,
+        },
+      ],
+      locale: "fr_FR",
+      type: "article",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [`https://www.vivieng.com${project.imageUrl}`], // Again, consider using a project-specific image
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+  };
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
