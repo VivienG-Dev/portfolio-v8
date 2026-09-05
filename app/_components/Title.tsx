4610d387f8e737245d3dc48ed461f505721d509b
VivienG-Dev <dean.lockheart@gmail.com>
2026-09-05 20:22:12 +0200

    fix: adjust Title component font size for improved readability
    - Reduced the font size of the title in the Title component from 4xl to 2xl for better visual hierarchy and consistency across the application.

--- app/_components/Title.tsx
+++ app/_components/Title.tsx
@@ -1,6 +1,6 @@
 export const Title = ({ title, as: Heading = "h2" }: { title: string; as?: "h1" | "h2" }) => (
   <div className="relative isolate pt-3 pl-4">
-    <span aria-hidden="true" className="pointer-events-none absolute left-4 top-0 -z-10 select-none text-4xl font-bold leading-tight tracking-tight text-black/[0.07] dark:text-white/[0.07] sm:left-2 sm:text-5xl">
+    <span aria-hidden="true" className="pointer-events-none absolute left-4 top-0 -z-10 select-none text-2xl font-bold leading-tight tracking-tight text-black/[0.07] dark:text-white/[0.07] sm:left-2 sm:text-5xl">
       {title}
     </span>
     <Heading className="relative text-3xl font-semibold leading-tight tracking-tight text-neutral-950 dark:text-primary">
